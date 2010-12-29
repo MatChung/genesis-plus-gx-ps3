a3d54b82222dd7be1814e244a0b02f56e72e0194
Squarepusher <danieldematteis@gmail.com>
2010-12-29 16:04:22 +0100

    All buttons should be mapped correctly again.

--- src/menu.h
+++ src/menu.h
@@ -116,18 +116,18 @@ char* do_pathmenu(uint16_t is_for_shader_or_dir_selection, const char * pathname
 //BEGINNING OF GENESIS PLUS PS3
 #define INPUT_NONE	(0x00000900)
 //END OF GENESIS PLUS PS3
-#define INPUT_MODE      (0x00000800)
-#define INPUT_Z         (0x00000400)
-#define INPUT_Y         (0x00000200)
-#define INPUT_X         (0x00000100)
-#define INPUT_START     (0x00000080)
-#define INPUT_C         (0x00000040)
-#define INPUT_B         (0x00000020)
-#define INPUT_A         (0x00000010)
-#define INPUT_LEFT      (0x00000004)
-#define INPUT_RIGHT     (0x00000008)
-#define INPUT_DOWN      (0x00000002)
-#define INPUT_UP        (0x00000001)
+#define INPUT_MODE      (0x0800)
+#define INPUT_X         (0x0400)
+#define INPUT_Y         (0x0200)
+#define INPUT_Z         (0x0100)
+#define INPUT_START     (0x0080)
+#define INPUT_A         (0x0040)
+#define INPUT_C         (0x0020)
+#define INPUT_B         (0x0010)
+#define INPUT_RIGHT     (0x0008)
+#define INPUT_LEFT      (0x0004)
+#define INPUT_DOWN      (0x0002)
+#define INPUT_UP        (0x0001)
 
 
 #endif /* MENU_H_ */
